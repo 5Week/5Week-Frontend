0949cb82378a7e012fe2f040613cbb049e6dba61
doublekm153 <doublekm153@gmail.com>
2023-08-18 00:38:59 +0900

    ㅇ

--- src/mainScreen/navigationBar/NavigationBar.jsx
+++ src/mainScreen/navigationBar/NavigationBar.jsx
@@ -5,8 +5,8 @@ import "./NavigationBar.css";
 const NavigationBar = () => {
   return (
     <div className="navigation">
-      <Link to="/" className="nav-link">꿈나눔testtestgg</Link>
-      <Link to="/login" className="login-button">로그인tdfdfest</Link>
+      <Link to="/" className="nav-link">꿈나눔</Link>
+      <Link to="/login" className="login-button">로그인</Link>
     </div>
   );
 };
